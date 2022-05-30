411569270d7d67953e000d2f66a1f022cdd62ca4
NxtGenLegend <62510055+NxtGenLegend@users.noreply.github.com>
2022-05-30 19:50:21 +0530

    Adding T & H

--- Raspberry Pi/Sensor Value Readings.py
+++ Raspberry Pi/Sensor Value Readings.py
@@ -1,1 +1,18 @@
+# Temperature = Kh / Humidity
+# Light  
+# 
+#
+#
+
+import sys
+import Adafruit_DHT
+
+HumPort = 
+TemPort = 
+
+humidity, temperature = Adafruit_DHT.read()
+
+for i in range(1, )
+
+kh = humidity * temperature
 
