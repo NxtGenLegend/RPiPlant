855af6c900352713d8012bd33687586a1b055f96
NxtGenLegend <62510055+NxtGenLegend@users.noreply.github.com>
2022-05-31 22:29:25 +0530

    Added Humidity & Temperature Function (Without Variance)

--- Raspberry Pi/Sensor Value Readings.py
+++ Raspberry Pi/Sensor Value Readings.py
@@ -5,14 +5,27 @@
 #
 
 import sys
+import time
+import numpy as np
 import Adafruit_DHT
 
-HumPort = 
-TemPort = 
+HumPort = 1
+TemPort = 2
+khVal = []
+flag = 0
 
-humidity, temperature = Adafruit_DHT.read()
-
-for i in range(1, )
-
-kh = humidity * temperature
-
+def humtemp():
+    for j in range (1, 1000):
+        for i in range(1, 10):
+            kh = Adafruit_DHT.read(HumPort) * Adafruit_DHT.read(TemPort)
+            khVal.append(kh)
+            time.sleep(10)
+        khAvg = np.mean(khVal)
+        for k in range(1, 10):
+            kh2 = Adafruit_DHT.read(HumPort) * Adafruit_DHT.read(TemPort)
+            if kh2 > kh or kh2 < kh:
+                flag += 1
+                if flag > 50:
+                    return
+                
+# Either exit all functions or trigger warning, depending on the logical flow
