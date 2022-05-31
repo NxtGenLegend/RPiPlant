8adb8b2c656c59dade6acc620ccff22cf35062dc
NxtGenLegend <62510055+NxtGenLegend@users.noreply.github.com>
2022-05-31 22:41:13 +0530

    Added MSE

--- Raspberry Pi/Sensor Value Readings.py
+++ Raspberry Pi/Sensor Value Readings.py
@@ -11,19 +11,24 @@ import Adafruit_DHT
 
 HumPort = 1
 TemPort = 2
-khVal = []
 flag = 0
+frames = 10
+khVal = []
+khErrorVal = []
 
 def humtemp():
     for j in range (1, 1000):
-        for i in range(1, 10):
+        for i in range(1, frames):
             kh = Adafruit_DHT.read(HumPort) * Adafruit_DHT.read(TemPort)
             khVal.append(kh)
+            khError = (kh - np.mean(khVal))^2
+            khErrorVal.append(khError)
             time.sleep(10)
         khAvg = np.mean(khVal)
-        for k in range(1, 10):
+        khErrorAvg = sum(khErrorVal) / frames
+        for k in range(1, frames):
             kh2 = Adafruit_DHT.read(HumPort) * Adafruit_DHT.read(TemPort)
-            if kh2 > kh or kh2 < kh:
+            if kh2 > khErrorAvg or kh2 < khErrorAvg:
                 flag += 1
                 if flag > 50:
                     return
